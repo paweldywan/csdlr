bad04968ba02bf4a60bd0e90789071092028ac09
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-19 00:15:56 +0200

    Add project files.

---
+++ Dogs/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dogs
+{
+    public class Dog
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+}
---
+++ csdlr/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csdlr
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine("Hi, my name is {0}", FirstName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = args;
+
+
+            //TestReflection();
+
+            //WriteSeparator();
+
+            TestDynamic();
+
+
+            Console.ReadLine();
+        }
+
+        private static object GetASpeaker()
+        {
+            return new Employee { FirstName = "Scott" };
+        }
+
+        private static void TestReflection()
+        {
+            object o = GetASpeaker();
+
+            o.GetType().GetMethod("Speak").Invoke(o, null);
+        }
+
+        private static void WriteSeparator()
+        {
+            Console.WriteLine("---");
+        }
+
+        private static void TestDynamic()
+        {
+            dynamic o = GetASpeaker();
+
+            o.Speak();
+        }
+    }
+}
