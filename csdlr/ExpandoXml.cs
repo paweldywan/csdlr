d26547630f6a2691a7af3d64390245952ee69c80
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-20 05:26:51 +0200

    Development

---
+++ csdlr/ExpandoXml.cs
@@ -0,0 +1,198 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace csdlr
+{
+    public static class ExpandoXml
+    {
+        /// <summary>
+        /// Utworzenie ExpandoObject dla zadanego dokumentu XML
+        /// </summary>
+        /// <param name="document">Dokument XML, na podstawie którego zostanie utworzony ExpandoObject</param>
+        /// <returns>ExpandoObject utworzony na podstawie zadanego dokumentu XML</returns>
+        public static dynamic AsExpando(this XDocument document)
+        {
+            return CreateExpando(document.Root); //Utworzenie ExpandoObject dla elementu najwyższego w hierarchi w zadanym dokumencie XML
+        }
+
+        /// <summary>
+        /// Utworzenie ExpandoObject dla zadanego elementu XML
+        /// </summary>
+        /// <param name="element">Element XML, na podstawie którego zostanie utworzony ExpandoObject</param>
+        /// <returns>ExpandoObject utworzony na podstawie zadanego elementu XML</returns>
+        private static dynamic CreateExpando(XElement element)
+        {
+            //Utworzenie instancji klasy ExpandoObject i zrzutowanie jej na interfejs IDictionary w którym kluczem jest string, a wartością object
+            var result = new ExpandoObject() as IDictionary<string, object>;
+
+            //Sprawdzenie czy jakikolwiek z elementów podrzędnych przekazanego elementu posiada podelementy - czy jakikolwiek element (węzeł) nie jest liściem
+            if (element.Elements().Any(e => e.HasElements))
+            {
+                var list = new List<ExpandoObject>(); //Utworzenie listy obiektów ExpandoObject
+
+                result.Add(element.Name.ToString(), list); //Dodanie do słownika nazwy przekazanego elementu i listy ExpandoObject
+
+                foreach (var childElement in element.Elements()) //Przejście po każdym dziecku przekazanego elementu
+                {
+                    //Dodanie do listy obiektu ExpandoObject utworzonego na podstawie danego podelementu przekazanego elementu
+                    list.Add(CreateExpando(childElement));
+                }
+            }
+            else //Wszystkie elementy podrzędne przekazanego elementu są liśćmi - nie posiadają podelementów
+            {
+                foreach (var leafElement in element.Elements()) //Przejście po każdym podelemencie (każdy jest liściem) przekazanego elementu
+                {
+                    result.Add(leafElement.Name.ToString(), leafElement.Value); //Dodanie do słownika nazwy elementu i jego wartości
+                }
+            }
+
+            return result; //Zwrócenie utworzonego ExpandoObject (słownika)
+        }
+
+        public static dynamic AsExpando2(this XDocument document)
+        {
+            return CreateExpando2(document.Root);
+        }
+
+        private static dynamic CreateExpando2(XElement element)
+        {
+            var result = new ExpandoObject() as IDictionary<string, object>;
+
+            if (element.HasElements)
+            {
+                var list = new List<ExpandoObject>();
+
+                result.Add(element.Name.LocalName, list);
+
+                foreach (var childElement in element.Elements())
+                {
+                    list.Add(CreateExpando2(childElement));
+                }
+            }
+            else
+            {
+                result.Add(element.Name.LocalName, element.Value);
+            }
+
+            return result;
+        }
+
+        public static dynamic ToExpando(this XDocument xDocument)
+        {
+            string jsonText = JsonConvert.SerializeXNode(xDocument);
+
+            dynamic dyn = JsonConvert.DeserializeObject<ExpandoObject>(jsonText);
+
+            return dyn;
+        }
+
+        public static dynamic Convert(this XDocument xDocument)
+        {
+            return Convert(xDocument.Root);
+        }
+
+        private static dynamic Convert(XElement parent)
+        {
+            dynamic output = new ExpandoObject();
+
+            output.Name = parent.Name.LocalName;
+            output.Value = parent.Value;
+
+            output.HasAttributes = parent.HasAttributes;
+
+            if (parent.HasAttributes)
+            {
+                output.Attributes = new List<KeyValuePair<string, string>>();
+
+                foreach (XAttribute attr in parent.Attributes())
+                {
+                    KeyValuePair<string, string> temp = new KeyValuePair<string, string>(attr.Name.LocalName, attr.Value);
+                    output.Attributes.Add(temp);
+                }
+            }
+
+            output.HasElements = parent.HasElements;
+
+            if (parent.HasElements)
+            {
+                output.Elements = new List<dynamic>();
+
+                foreach (XElement element in parent.Elements())
+                {
+                    dynamic temp = Convert(element);
+                    output.Elements.Add(temp);
+                }
+            }
+
+            return output;
+        }
+
+        public static dynamic Parse(this XDocument xDocument)
+        {
+            dynamic parent = new ExpandoObject();
+
+            Parse(parent, xDocument.Root);
+
+            return parent;
+        }
+
+        public static void Parse(dynamic parent, XElement node)
+        {
+            if (node.HasElements)
+            {
+                if (node.Elements(node.Elements().First().Name.LocalName).Count() > 1)
+                {
+                    //list
+                    var item = new ExpandoObject();
+                    var list = new List<dynamic>();
+                    foreach (var element in node.Elements())
+                    {
+                        Parse(list, element);
+                    }
+
+                    AddProperty(item, node.Elements().First().Name.LocalName, list);
+                    AddProperty(parent, node.Name.ToString(), item);
+                }
+                else
+                {
+                    var item = new ExpandoObject();
+
+                    foreach (var attribute in node.Attributes())
+                    {
+                        AddProperty(item, attribute.Name.ToString(), attribute.Value.Trim());
+                    }
+
+                    //element
+                    foreach (var element in node.Elements())
+                    {
+                        Parse(item, element);
+                    }
+
+                    AddProperty(parent, node.Name.ToString(), item);
+                }
+            }
+            else
+            {
+                AddProperty(parent, node.Name.ToString(), node.Value.Trim());
+            }
+        }
+
+        private static void AddProperty(dynamic parent, string name, object value)
+        {
+            if (parent is List<dynamic>)
+            {
+                (parent as List<dynamic>).Add(value);
+            }
+            else
+            {
+                (parent as IDictionary<string, object>)[name] = value;
+            }
+        }
+    }
+}
--- csdlr/Program.cs
+++ csdlr/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace csdlr
 {
@@ -25,15 +28,31 @@ namespace csdlr
             _ = args;
 
 
-            //TestReflection();
+            TestReflection();
 
-            //WriteSeparator();
+            WriteSeparator();
 
-            //TestDynamic();
+            TestDynamic();
 
-            //WriteSeparator();
+            WriteSeparator();
 
             TestExcel();
+
+            WriteSeparator();
+
+            TestExpandoObject();
+
+            WriteSeparator();
+
+            GenerateXML();
+
+            WriteSeparator();
+
+            DumpEmployees();
+
+            WriteSeparator();
+
+            TestDynamicObject();
 
 
             Console.ReadLine();
@@ -92,6 +111,150 @@ namespace csdlr
 
                 sheet.Cells[i + 1, "B"] = processes[i].Threads.Count;
             }
+        }
+
+        private static void TestExpandoObject()
+        {
+            dynamic expando = new ExpandoObject();
+
+            expando.Name = "Scott";
+
+            expando.Speak = new Action(() => Console.WriteLine(expando.Name));
+
+
+            expando.Speak();
+
+
+            foreach (var memeber in expando)
+            {
+                Console.WriteLine(memeber.Key);
+            }
+        }
+
+        private static void GenerateXML()
+        {
+            var employees = new[] { "Scott", "Poonam", "Paul", "Karthik", "Nirhir" };
+
+            XDocument xDocument = new XDocument(
+                    new XElement("Employees",
+                        employees.Select(e =>
+                            new XElement("Employee",
+                                new XElement("FirstName", e)
+                            ))
+                    ));
+
+            xDocument.Save("Employees.xml");
+        }
+
+        private static void DumpEmployees()
+        {
+            var doc = XDocument.Load("Employees.xml");
+
+            foreach (var element in doc.Element("Employees")
+                                       .Elements("Employee"))
+            {
+                Console.WriteLine(element.Element("FirstName").Value);
+            }
+
+
+            var doc2 = doc.AsExpando();
+
+            foreach (var employee in doc2.Employees)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+
+
+            var doc3 = doc.ToExpando();
+
+            foreach (var employee in doc3.Employees.Employee)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+
+
+            var doc4 = doc.Convert();
+
+            foreach (var employee in doc4.Elements)
+            {
+                foreach (var firstName in employee.Elements)
+                {
+                    Console.WriteLine(firstName.Value);
+                }
+            }
+
+
+            var doc5 = doc.AsExpando2();
+
+            foreach (var employee in doc5.Employees)
+            {
+                foreach (var firstName in employee.Employee)
+                {
+                    Console.WriteLine(firstName.FirstName);
+                };
+            }
+
+
+            var doc6 = doc.Parse();
+
+            foreach (var employee in doc6.Employees.Employee)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+        }
+
+        private static void TestDynamicObject()
+        {
+            dynamic doc = new DynamicXml("Employees.xml");
+
+            foreach (var employee in doc.Employees)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+        }
+    }
+
+    internal class DynamicXml : DynamicObject, IEnumerable
+    {
+        private readonly dynamic _xml;
+
+        public DynamicXml(string fileName)
+        {
+            _xml = XDocument.Load(fileName);
+        }
+
+        public DynamicXml(dynamic xml)
+        {
+            _xml = xml;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var xml = _xml.Element(binder.Name);
+
+            if (xml != null)
+            {
+                result = new DynamicXml(xml);
+
+                return true;
+            }
+
+            result = null;
+
+            return false;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var child in _xml.Elements())
+            {
+                yield return new DynamicXml(child);
+            }
+        }
+
+        public static implicit operator string (DynamicXml xml)
+        {
+            return xml._xml.Value;
         }
     }
 }
